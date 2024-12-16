3b65b22ee0c5f0b53d0236f0b08f91abca14ee8a
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-12-16 06:41:31 +0100

    Add: datatypes for numbers

--- Program.cs
+++ Program.cs
@@ -1,9 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("My name is emmanuel");
-int num = 30;
-long BigNum = 89099960L;
 
-Console.WriteLine(num);
+int age = 30;
+Console.WriteLine(age);
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(int.MinValue);
+
+long BigNum = 89099960L;
 Console.WriteLine(BigNum);
+Console.WriteLine(long.MaxValue);
+Console.WriteLine(long.MinValue);
+
+float precision = 5.000200F;
+Console.WriteLine(precision);
+Console.WriteLine(float.MaxValue);
+Console.WriteLine(float.MinValue);
+
+double num1 = 65.87D;
+Console.WriteLine(num1);
+Console.WriteLine(double.MaxValue);
+Console.WriteLine(double.MinValue);
+
+decimal money = 14.99M;
+Console.WriteLine(money);
+Console.WriteLine(decimal.MaxValue);
+Console.WriteLine(decimal.MinValue);
+
+
 Console.ReadLine();
