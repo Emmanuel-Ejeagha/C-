d3c97e1e44f2c4b74efe8da551bfc1e2136f45c1
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-12-16 06:41:50 +0100

    Add: datatypes for numbers

--- obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4cdd75ad2aa9766a84b9639c4a93f9255738776")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38cbc6a5c1859b59af1c0438b9d8fcff80ac3d99")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
