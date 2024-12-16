f6b4d76f38e9b0635dd60845dc99867cec04dfb0
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-12-16 01:01:05 +0100

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is emmanuel");
+int num = 30;
+long BigNum = 89099960L;
+
+Console.WriteLine(num);
+Console.WriteLine(BigNum);
+Console.ReadLine();
