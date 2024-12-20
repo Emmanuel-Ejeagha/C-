eff81c208b0d169ddf4efc7695b7953812d6893b
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-12-20 16:09:04 +0100

    Add: if statement

--- obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ad34d4b463a9d4b38a42e9032010efdbc8192b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03dc85abef613d3edff23e90f44f07cb3ccd9a23")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
