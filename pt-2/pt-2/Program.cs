12e4d75d7b781f32089c0fc72c4509e3be552261
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-12-25 15:47:40 +0100

    Add: do while loop

---
+++ pt-2/pt-2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Formats.Asn1;
+
+Console.Write("Enter the first number: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int answer = numA * numB;
+int actualAnswer = 0;
+
+do
+{
+    Console.Write("Enter your answer: ");
+    actualAnswer = Convert.ToInt32(Console.ReadLine());
+
+    if (answer != actualAnswer)
+    {
+        Console.WriteLine("Close but it was wrong!");
+
+    }
+} while (answer != actualAnswer);
+    Console.WriteLine("Well done!");
