72d0b25c8ce9d8cf2481e527667a5c66030a91f3
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-12-27 19:55:59 +0100

    Add: unicode

--- pt-2/pt-2/Program.cs
+++ pt-2/pt-2/Program.cs
@@ -72,3 +72,17 @@ Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
 Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
 // User command to run an application
 Console.WriteLine(@"c:\invoices\app.exe -j");
+
+// string projectName = "ACME";
+
+// string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+// Console.WriteLine("\n" + projectName + "\n");
+// Console.WriteLine(russianMessage);
+
+string projectName = "ACME";
+string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
