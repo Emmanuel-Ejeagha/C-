76d83a24a5c22732b5ba2d33c403bf76bc8194cb
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-12-28 14:14:56 +0100

    Add: GPA calculator

--- pt-2/pt-2/Program.cs
+++ pt-2/pt-2/Program.cs
@@ -91,3 +91,74 @@ string firstName = "Bob";
 int widgetsSold = 7;
 Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
 Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
+
+// In math, PEMDAS is an acronym that helps students remember the order of operations. The order is:
+
+// Parentheses (whatever is inside the parenthesis is performed first)
+// Exponents
+// Multiplication and Division (from left to right)
+// Addition and Subtraction (from left to right)
+
+// Calculating a degree celcius
+int fahrenheit = 94;
+decimal result1 = (fahrenheit - 32m) * (5m/9m);
+
+Console.WriteLine($"The temperature {result1} Celsius.");
+
+// GPA Calculator
+string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
+
+int gradeA = 4;
+int gradeB = 3;
+
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+
+int totalCreditHours = 0;
+totalCreditHours += course1Credit;
+totalCreditHours += course2Credit;
+totalCreditHours += course3Credit;
+totalCreditHours += course4Credit;
+totalCreditHours += course5Credit;
+
+int totalGradePoints = 0;
+totalGradePoints += course1Credit * course1Grade;
+totalGradePoints += course2Credit * course2Grade;
+totalGradePoints += course3Credit * course3Grade;
+totalGradePoints += course4Credit * course4Grade;
+totalGradePoints += course5Credit * course5Grade;
+
+// Console.WriteLine($"{totalGradePoints} {totalCreditHours}");
+decimal gradePointAverage = (decimal) totalGradePoints/totalCreditHours;
+
+Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
+Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
+Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
+Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
+Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
+
+Console.WriteLine($"Final GPA: {gradePointAverage}");
