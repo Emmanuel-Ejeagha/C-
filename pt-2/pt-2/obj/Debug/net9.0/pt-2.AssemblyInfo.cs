34819e5de56ca4a40a3acb540caaae6937f1d575
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-12-29 21:39:11 +0100

    Add: cSharp lib

--- pt-2/pt-2/obj/Debug/net9.0/pt-2.AssemblyInfo.cs
+++ pt-2/pt-2/obj/Debug/net9.0/pt-2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pt-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67079854d6b2dc34d08c07452bf1988dadc42127")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50e45d96cd526240d0f73447c37b2cee0c6e56c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("pt-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pt-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
