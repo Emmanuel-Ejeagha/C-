ca6927a166fe5def1c46745c7374737b5b0829e0
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-12-30 03:36:54 +0100

    Add: if statement

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -25,3 +25,12 @@ int roll3 = dice.Next(1, 7);
 int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+else 
+{
+    Console.WriteLine("Sorry, you lose.");
+}
