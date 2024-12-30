2024a13e954f2b99f91eac5f95ca6c574b388114
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-12-30 15:00:06 +0100

    Add: Nexted if else statement

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,11 +1,11 @@
-﻿// Random dice = new Random();
-// int roll1 = dice.Next();
-// int roll2 = dice.Next(101);
-// int roll3 = dice.Next(50, 101);
+﻿Random dice = new Random();
+int roll001 = dice.Next();
+int roll002 = dice.Next(101);
+int roll003 = dice.Next(50, 101);
 
-// Console.WriteLine($"First roll: {roll1}");
-// Console.WriteLine($"Second roll: {roll2}");
-// Console.WriteLine($"Third roll: {roll3}");
+Console.WriteLine($"First roll: {roll001}");
+Console.WriteLine($"Second roll: {roll002}");
+Console.WriteLine($"Third roll: {roll003}");
 
 int firstValue = 500;
 int secondValue = 600;
@@ -13,25 +13,25 @@ int largerValue;
 
 largerValue = Math.Max(firstValue, secondValue);
 
-Console.WriteLine(largerValue.Equals(secondValue));
+Console.WriteLine(largerValue);
 
 // Roll a dice
-Random dice = new Random();
+Random dice1 = new Random();
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+int roll01 = dice1.Next(1, 7);
+int roll02 = dice1.Next(1, 7);
+int roll03 = dice1.Next(1, 7);
 
-int total = roll1 + roll2 + roll3;
+int total = roll01 + roll02 + roll03;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+Console.WriteLine($"Dice roll: {roll01} + {roll02} + {roll03} = {total}");
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+if ((roll01 == roll02) || (roll02 == roll03) || (roll01 == roll03))
 {
     Console.WriteLine("You rolled doubles! +2 bonus to total");
     total += 2;
 }
-if ((roll1 == roll2) && (roll2 == roll3))
+if ((roll01 == roll02) && (roll02 == roll03))
 {
     Console.WriteLine("You rolled triples! +6 bonus to total!");
     total += 6;
@@ -53,4 +53,39 @@ Console.WriteLine(result);
 if (message.Contains("fox"))
 {
     Console.WriteLine("What does the fox say?");
+}
+Console.WriteLine("\n");
+
+// Nexted if statement
+Random dice2 = new Random();
+
+int roll1 = dice2.Next(1, 7);
+int roll2 = dice2.Next(1, 7);
+int roll3 = dice2.Next(1, 7);
+
+int total1 = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3));
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total1 += 6; 
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total1 += 2;
+    }
+}
+
+if (total1 >= 15)
+{
+    Console.WriteLine("You win!");
+}
+else
+{
+    Console.WriteLine("Sorry, you lose.");
 }
