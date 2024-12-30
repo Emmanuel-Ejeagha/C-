bda88faab00c88ad6305e09b3661d56ad42d50b4
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-12-30 03:37:43 +0100

    Add: if statement

--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34819e5de56ca4a40a3acb540caaae6937f1d575")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0847304aa872d0c8afcbf09d6f6f9d20becaaebf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
