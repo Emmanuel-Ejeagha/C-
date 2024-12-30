cea596c764063e99bde06610585acccb2d546174
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-12-30 03:58:34 +0100

    Add: boolean expression

--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0847304aa872d0c8afcbf09d6f6f9d20becaaebf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e984c3b5e74f8df4f90b1b86c2022bc21f75ea4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
