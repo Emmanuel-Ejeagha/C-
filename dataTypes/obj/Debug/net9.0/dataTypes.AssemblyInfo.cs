79fb227ee9dc3390e0ecc8addce3f2b05be28a67
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-05 03:26:44 +0100

    Add: cast and conversion

--- dataTypes/obj/Debug/net9.0/dataTypes.AssemblyInfo.cs
+++ dataTypes/obj/Debug/net9.0/dataTypes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dataTypes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af530b1e5c2695a81eab359b6fd4eb3fcd7af42c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c53f120677654524254c266da097ec0d83c0ed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("dataTypes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dataTypes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
