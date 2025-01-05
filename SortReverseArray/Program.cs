a3d8dd3c8f0603df034e153b78b832337136f44d
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-05 20:45:44 +0100

    Add: string interpolation

--- SortReverseArray/Program.cs
+++ SortReverseArray/Program.cs
@@ -138,3 +138,87 @@ string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} pri
 
 yourDiscount = $"A discount of {((price - salePrice)/price):P2}!";
 Console.WriteLine(yourDiscount);
+
+// string interpolation
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
+
+// padding
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(10));
+Console.WriteLine(input.PadLeft(12, '*'));
+Console.WriteLine(input.PadRight(12, '-'));
+
+string paymentID = "769C";
+
+var formattedLine = paymentID.PadLeft(6);
+
+Console.WriteLine(formattedLine);
+
+// Add the payee name to the output
+paymentID = "R645";
+string payeeName = "Mr. Christopher Colombus";
+string paymentAmt = "$5,000.00";
+
+var formattedLine1 = paymentID.PadRight(6);
+formattedLine1 += payeeName.PadRight(24);
+formattedLine1 += paymentAmt.PadLeft(15);
+
+Console.WriteLine("\n11234550987654321234566543223457654323456777767543");
+Console.WriteLine(formattedLine1);
+
+// Complete a challenge to apply string interpolation to a form letter
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+Console.WriteLine(comparisonMessage);
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+Console.WriteLine(comparisonMessage);
+
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+Console.WriteLine(comparisonMessage);
+
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+Console.WriteLine(comparisonMessage);
