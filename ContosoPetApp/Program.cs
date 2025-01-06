7b6ed4408d3892c767770d78fdef04a790d64817
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-06 15:47:49 +0100

    Add: Modified

--- ContosoPetApp/Program.cs
+++ ContosoPetApp/Program.cs
@@ -136,8 +136,39 @@ do
                     }
                 }
             }
-            Console.WriteLine("\n\rPress the Enter key to continue");
+            // Display all dogs with a specified characteristic
+            string dogCharacteristic = "";
+
+            while (dogCharacteristic == "")
+            {
+                // have the user enter physical characteristics to search for
+                Console.WriteLine($"\nEnter one desired dog characteristics to search for");
+                readResult = Console.ReadLine();
+                if (readResult != null)
+                {
+                    dogCharacteristic = readResult.ToLower().Trim();
+                }
+            } 
+            Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
+            bool noMatchesDog = true;
+            // #6 loop through the ourAnimals array to search for matching animals
+            string dogDescription = "";
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 1].Contains("dog"))
+                {
+                    // #7 Search combined descriptions and report results
+                    dogDescription = ourAnimals[i, 4] + "\n" + ourAnimals[i, 5];
+                    if (dogDescription.Contains(dogCharacteristic))
+                    {
+                        Console.WriteLine($"\nOur dog {ourAnimals[i, 3]} is a match!");
+                        Console.WriteLine(dogDescription);
+
+                        noMatchesDog = false;
+                    }
+                }
+            }
             break;
         
         case "2":
@@ -164,7 +195,11 @@ do
                 //  get species (cat or dog) - string animalSpecies is a required field
                 do
                 {
-                    Console.WriteLine("n\rEnter 'dog' or 'cat' to begin a new entry");
+                    if (noMatchesDog)
+                    {
+                        Console.WriteLine("None of our dogs are a match found for: " + dogCharacteristic);
+                    }
+                    Console.WriteLine("\n\rEnter 'dog' or 'cat' to begin a new entry");
                     readResult = Console.ReadLine();
                     if (readResult != null)
                     {
