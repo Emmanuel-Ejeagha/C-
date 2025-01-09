515d8ead709d54713c4e4346e2823a2733a4cf80
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-09 16:21:18 +0100

    Modified

--- HelloC#/obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ HelloC#/obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a2f953f6026c4e95ffabbc95c643927650d1a99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c23caed69a64b84390800422b132eb3ffd8049eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
