17ef6e6205f4193d38f117dd173d5e7b4447ed38
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-02-11 17:41:40 +0100

    Add: Attributes

---
+++ obsolete/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+public class MainClass
+{
+
+    private static void Main()
+    {
+        Calculator.Add(10, 50);
+    }
+}
+
+public class Calculator
+{
+    [Obsolete("Use Add(List<int> Numbers) Method")]
+    public static int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public static int Add(List<int> Numbers)
+    {
+        int Sum = 0;
+        foreach (int Number in Numbers)
+        {
+            Sum += Number;
+        }
+        return Sum;
+    }
+}
+
+
